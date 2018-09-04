236a71152d2aa8c520f7133759b238841d74126b
Scott Addie <scott.addie@microsoft.com>
2018-09-04 17:04:54 -0500

    Sample project cleanup

--- TelerikAspNetCoreApp1/Controllers/HomeController.cs
+++ TelerikAspNetCoreApp1/Controllers/HomeController.cs
@@ -1,19 +1,9 @@
-﻿using Kendo.Mvc.Extensions;
-using Kendo.Mvc.UI;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TelerikAspNetCoreApp1.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace TelerikAspNetCoreApp1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
     }
 }
